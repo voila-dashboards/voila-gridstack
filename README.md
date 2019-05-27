428adafd2d2eecade1950bee35d4762d21f8b738
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-05-27 23:21:56 +0200

    Initial commit for the voila gridstack template.

---
+++ README.md
@@ -0,0 +1,11 @@
+# voila-gridstack
+
+Gridstack template for voila.
+
+## License
+
+We use a shared copyright model that enables all contributors to maintain the
+copyright on their contributions.
+
+This software is licensed under the BSD-3-Clause license. See the
+[LICENSE](LICENSE) file for details.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+data_files = []
+for root, dirs, files in os.walk('share'):
+    root_files = [os.path.join(root, i) for i in files]
+    data_files.append((root, root_files))
+
+setup_args = {
+    'name': 'voila-gridstack',
+    'version': '0.0.1',
+    'packages': [],
+    'data_files': data_files,
+    'install_requires': [
+        'voila'
+    ],
+    'author': 'QuantStack',
+    'author_email': 'info@quantstack.net',
+    'url': 'https://github.com/QuantStack/voila-gridstack/'
+}
+
+if __name__ == '__main__':
+    setup(**setup_args)
