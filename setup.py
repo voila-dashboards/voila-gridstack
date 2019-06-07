dcb4620f267a54491c8f18b9f29b0330842e9a7e
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-06-08 00:44:48 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ for root, dirs, files in os.walk('share'):
 
 setup_args = {
     'name': 'voila-gridstack',
-    'version': '0.0.1',
+    'version': '0.0.2',
     'packages': [],
     'data_files': data_files,
     'install_requires': [
