a027e435bab1bee032eb106260fc9657ad21e93b
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2019-10-31 11:51:54 +0100

    test showing/hiding gs handles

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -5,15 +5,23 @@ from lxml import etree
 
 BASE_DIR = os.path.dirname(__file__)
 
+@pytest.fixture(params=[False, True], ids="show_handles={}".format)
+def show_handles(request):
+    return request.param
+
 @pytest.fixture
-def voila_args():
+def voila_resources(show_handles):
+    return {"gridstack": {"show_handles": show_handles}}
+
+@pytest.fixture
+def voila_args(voila_resources):
     nb_path = os.path.join(BASE_DIR, 'nb.ipynb')
     return [nb_path,
             '--VoilaTest.config_file_paths=[]',
-            '--VoilaConfiguration.resources={"gridstack": {"show_handles": True}}']
+            '--VoilaConfiguration.resources={}'.format(voila_resources)]
 
 @pytest.mark.gen_test
-def test_resources_gridstack_show_handles(http_client, base_url):
+def test_resources_gridstack_show_handles(http_client, base_url, show_handles):
     """Test with handlebars"""
     response = yield http_client.fetch(base_url)
     assert response.code == 200
@@ -23,4 +31,11 @@ def test_resources_gridstack_show_handles(http_client, base_url):
     tree = etree.fromstring(html_body, parser=parser)
 
     elem = tree.xpath("//div[@class='gridhandle']")
-    assert elem
+    assert (show_handles and elem) or (not show_handles and not elem)
+
+    gridstack_config = tree.xpath("//script[contains(text(), 'gridstack')]")[0].text
+
+    if show_handles:
+        assert "handles: 'e, se, s, sw, w'" in gridstack_config
+    else:
+        assert "handles: 'none'" in gridstack_config
