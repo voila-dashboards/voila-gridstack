f6553354874633693a148ab77a85032dd0386c85
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-09-17 08:21:20 +0200

    Release 0.0.10

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ def get_data_files():
 
 setup_args = {
     'name': 'voila-gridstack',
-    'version': '0.1.0a7',
+    'version': '0.0.10',
     'packages': find_packages(),
     'data_files': get_data_files(),
     'package_data': {
