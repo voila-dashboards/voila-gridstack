ed4ccb13aae1622375ad1fde82a4825df3d02630
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-10-21 16:44:06 +0200

    Use ToolbarButtonComponent for the save icon

--- src/editor/toolbar/save.tsx
+++ src/editor/toolbar/save.tsx
@@ -1,4 +1,4 @@
-import { ReactWidget } from '@jupyterlab/apputils';
+import { ReactWidget, ToolbarButtonComponent } from '@jupyterlab/apputils';
 
 import { saveIcon } from '@jupyterlab/ui-components';
 
@@ -9,28 +9,20 @@ import EditorPanel from '../panel';
 export default class Save extends ReactWidget {
   constructor(panel: EditorPanel) {
     super();
-    this.addClass('jp-ToolbarButton');
     this._panel = panel;
   }
 
-  private save(): void {
-    this._panel.save();
-  }
-
   render(): JSX.Element {
+    const onClick = (): void => {
+      this._panel.save();
+    };
+
     return (
-      <div
-        onClick={() => this.save()}
-        className="bp3-button bp3-minimal jp-ToolbarButtonComponent minimal jp-Button"
-      >
-        <saveIcon.react
-          tag="span"
-          right="7px"
-          top="5px"
-          width="16px"
-          height="16px"
-        />
-      </div>
+      <ToolbarButtonComponent
+        icon={saveIcon}
+        onClick={onClick}
+        tooltip={'Save the layout'}
+      />
     );
   }
 
