9e09d8a257f9da62a7211635c6f49540e6c46cdc
hbcarlos <carlosherrerocontact@gmail.com>
2020-10-29 13:53:40 +0100

    New voila and voila editor buttons

---
+++ src/editor/toolbar/voila.tsx
@@ -0,0 +1,34 @@
+import { ReactWidget, ToolbarButtonComponent } from '@jupyterlab/apputils';
+
+import { PageConfig } from '@jupyterlab/coreutils';
+
+import * as React from 'react';
+
+export default class Voila extends ReactWidget {
+  constructor(path: string) {
+    super();
+    this._path = path;
+  }
+
+  render(): JSX.Element {
+    const onClick = (): void => {
+      const baseUrl = PageConfig.getBaseUrl();
+      const conf = '?voila-template=gridstack';
+      const win = window.open(
+        `${baseUrl}voila/render/${this._path + conf}`,
+        '_blank'
+      );
+      win.focus();
+    };
+
+    return (
+      <ToolbarButtonComponent
+        iconClass="jp-MaterialIcon jp-VoilaIcon"
+        onClick={onClick}
+        tooltip={'Open voila'}
+      />
+    );
+  }
+
+  private _path: string;
+}
