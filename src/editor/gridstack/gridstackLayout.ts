8922ca591bb0eb4d0f27780a808c1763ba6c6940
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-11-25 15:31:09 +0100

    Quick pass on typing and null checks

--- src/editor/gridstack/gridstackLayout.ts
+++ src/editor/gridstack/gridstackLayout.ts
@@ -34,9 +34,45 @@ export class GridStackLayout extends Layout {
     this._cellHeight = info.defaultCellHeight;
     this._columns = info.maxColumns;
 
-    this._gridItems = new Array<GridStackItem>();
+    this._margin = info.cellMargin;
+    this._cellHeight = info.defaultCellHeight;
+    this._columns = info.maxColumns;
 
-    this._gridItemChanged = new Signal<this, GridStackNode[]>(this);
+    this._gridHost = document.createElement('div');
+    this._gridHost.className = 'grid-stack';
+
+    this._grid = GridStack.init(
+      {
+        float: true,
+        column: this._columns,
+        margin: this._margin,
+        cellHeight: this._cellHeight,
+        styleInHead: true,
+        disableOneColumnMode: true,
+        draggable: { handle: '.grid-item-toolbar' },
+        resizable: { autoHide: true, handles: 'e, se, s, sw, w' },
+        alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      },
+      this._gridHost
+    );
+
+    this._grid.on(
+      'change',
+      (event: Event, items: GridHTMLElement | GridStackNode[] | undefined) => {
+        this._onChange(event, items as GridStackNode[]);
+      }
+    );
+
+    this._grid.on(
+      'removed',
+      (event: Event, items: GridHTMLElement | GridStackNode[] | undefined) => {
+        if ((items as GridStackNode[]).length <= 1) {
+          this._onRemoved(event, items as GridStackNode[]);
+        }
+      }
+    );
   }
 
   get gridItemChanged(): ISignal<this, GridStackNode[]> {
@@ -47,20 +83,26 @@ export class GridStackLayout extends Layout {
    * Dispose of the resources held by the widget.
    */
   dispose(): void {
-    this._gridItems = undefined;
-    this._grid?.destroy();
-    this._grid = undefined;
+    this._grid.destroy();
     super.dispose();
+  }
+
+  /**
+   * Init the gridstack layout
+   */
+  init(): void {
+    super.init();
+    this.parent!.node.appendChild(this._gridHost);
   }
 
   /**
    * Handle `update-request` messages sent to the widget.
    */
-  protected onUpdateRequest(msg: Message) {
+  protected onUpdateRequest(msg: Message): void {
     const items = this._grid?.getGridItems();
     items?.forEach(item => {
-      this._grid?.removeWidget(item, true, false);
-      this._grid?.addWidget(item);
+      this._grid.removeWidget(item, true, false);
+      this._grid.addWidget(item);
     });
   }
 
@@ -68,21 +110,21 @@ export class GridStackLayout extends Layout {
    * Handle `resize-request` messages sent to the widget.
    */
   protected onResize(msg: Message): void {
-    this._grid?.onParentResize();
+    this._grid.onParentResize();
   }
 
   /**
    * Handle `fit-request` messages sent to the widget.
    */
   protected onFitRequest(msg: Message): void {
-    this._grid?.onParentResize();
+    this._grid.onParentResize();
   }
 
   /**
    * Create an iterator over the widgets in the layout.
    */
   iter(): IIterator<Widget> {
-    return new ArrayIterator(this._gridItems ?? []);
+    return new ArrayIterator(this._gridItems);
   }
 
   /**
@@ -109,7 +151,7 @@ export class GridStackLayout extends Layout {
   set margin(margin: number) {
     if (this._margin !== margin) {
       this._margin = margin;
-      this._grid?.margin(this._margin);
+      this._grid.margin(this._margin);
       this.parent!.update();
     }
   }
@@ -120,7 +162,7 @@ export class GridStackLayout extends Layout {
    * @param forcePixel - An optional boolean.
    */
   get cellHeight(): number {
-    return this._grid?.getCellHeight(true) ?? this._cellHeight;
+    return this._grid.getCellHeight(true) ?? this._cellHeight;
   }
 
   /**
@@ -131,7 +173,7 @@ export class GridStackLayout extends Layout {
   set cellHeight(height: number) {
     if (this._cellHeight !== height) {
       this._cellHeight = height;
-      this._grid?.cellHeight(this._cellHeight);
+      this._grid.cellHeight(this._cellHeight);
       this.parent!.update();
     }
   }
@@ -151,7 +193,7 @@ export class GridStackLayout extends Layout {
   set columns(columns: number) {
     if (this._columns !== columns) {
       this._columns = columns;
-      this._grid?.column(columns);
+      this._grid.column(columns);
       this.parent!.update();
     }
   }
@@ -160,62 +202,14 @@ export class GridStackLayout extends Layout {
    * Get the list of `GridStackItem` (Lumino widgets).
    */
   get gridWidgets(): Array<GridStackItem> {
-    return this._gridItems ?? [];
+    return this._gridItems;
   }
 
   /**
    * Get the list of `GridItemHTMLElement`.
    */
   get gridItems(): GridItemHTMLElement[] {
-    return this._grid?.getGridItems() ?? [];
-  }
-
-  /**
-   * Initialize gridstack.
-   *
-   * @param info - The dashboard metadata parameters.
-   */
-  initGridStack(info: DashboardView): void {
-    this._margin = info.cellMargin;
-    this._cellHeight = info.defaultCellHeight;
-    this._columns = info.maxColumns;
-
-    const grid = document.createElement('div');
-    grid.className = 'grid-stack';
-    this.parent!.node.appendChild(grid);
-
-    this._grid = GridStack.init(
-      {
-        float: true,
-        column: this._columns,
-        margin: this._margin,
-        cellHeight: this._cellHeight,
-        styleInHead: true,
-        disableOneColumnMode: true,
-        draggable: { handle: '.grid-item-toolbar' },
-        resizable: { autoHide: true, handles: 'e, se, s, sw, w' },
-        alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-          navigator.userAgent
-        )
-      },
-      grid
-    );
-
-    this._grid.on(
-      'change',
-      (event: Event, items: GridHTMLElement | GridStackNode[] | undefined) => {
-        this._onChange(event, items as GridStackNode[]);
-      }
-    );
-
-    this._grid.on(
-      'removed',
-      (event: Event, items: GridHTMLElement | GridStackNode[] | undefined) => {
-        if ((items as GridStackNode[]).length <= 1) {
-          this._onRemoved(event, items as GridStackNode[]);
-        }
-      }
-    );
+    return this._grid.getGridItems() ?? [];
   }
 
   /**
@@ -239,8 +233,8 @@ export class GridStackLayout extends Layout {
       options['autoPosition'] = true;
     }
 
-    this._gridItems?.push(item);
-    this._grid?.addWidget(item.node, options);
+    this._gridItems.push(item);
+    this._grid.addWidget(item.node, options);
   }
 
   /**
@@ -250,9 +244,9 @@ export class GridStackLayout extends Layout {
    * @param info - The dashboard cell metadata parameters.
    */
   updateGridItem(id: string, info: DashboardCellView): void {
-    const items = this._grid?.getGridItems();
+    const items = this._grid.getGridItems();
     const item = items?.find(value => value.gridstackNode?.id === id);
-    this._grid!.update(item!, info.col, info.row, info.width, info.height);
+    this._grid.update(item!, info.col, info.row, info.width, info.height);
   }
 
   /**
@@ -261,12 +255,12 @@ export class GridStackLayout extends Layout {
    * @param id - The Cell id.
    */
   removeGridItem(id: string): void {
-    const items = this._grid?.getGridItems();
+    const items = this._grid.getGridItems();
     const item = items?.find(value => value.gridstackNode?.id === id);
 
     if (item) {
-      this._gridItems = this._gridItems?.filter(obj => obj.cellId !== id);
-      this._grid?.removeWidget(item, true, false);
+      this._gridItems = this._gridItems.filter(obj => obj.cellId !== id);
+      this._grid.removeWidget(item, true, false);
     }
   }
 
@@ -289,7 +283,8 @@ export class GridStackLayout extends Layout {
   private _margin: number;
   private _cellHeight: number;
   private _columns: number;
-  private _grid: GridStack | undefined = undefined;
-  private _gridItems: Array<GridStackItem> | undefined = undefined;
-  private _gridItemChanged: Signal<this, GridStackNode[]>;
+  private _gridHost: HTMLElement;
+  private _grid: GridStack;
+  private _gridItems: GridStackItem[] = [];
+  private _gridItemChanged = new Signal<this, GridStackNode[]>(this);
 }
