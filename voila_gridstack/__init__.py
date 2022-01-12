b44f697fab531521503dc23d2da180069987e6f3
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-01-12 18:12:17 +0000

    Publish 0.3.0b1
    SHA256 hashes:
    
    voila-dashboards-jupyterlab-gridstack-0.3.0-beta.1.tgz: 33743d18793a97f045b4b9766daa58c0770f3d913c537881d273430e54874c8c
    
    voila-gridstack-0.3.0b1.tar.gz: 3d33bc13d55611ca03a523268c3ed245b9d007f5aaba7624c5805d51b6c12762
    
    voila_gridstack-0.3.0b1-py2.py3-none-any.whl: db38b8e380808e65ad69d272a6d96f797aac2b85e974574e2f62b44516061278

--- voila_gridstack/__init__.py
+++ voila_gridstack/__init__.py
@@ -11,7 +11,7 @@ import json
 import os.path as osp
 from .server_extension import load_jupyter_server_extension  # noqa
 
-__version__ = "0.3.0b0"
+__version__ = "0.3.0b1"
 
 HERE = osp.abspath(osp.dirname(__file__))
 
