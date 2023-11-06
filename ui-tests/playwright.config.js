34963a1b6e10f6d2d5141bae029cf1ad0eac6044
Duc Trung Le <leductrungxf@gmail.com>
2023-11-06 13:19:41 +0100

    add ui tests

---
+++ ui-tests/playwright.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  timeout: 120000,
+  reporter: [[process.env.CI ? 'dot' : 'list'], ['html']],
+  use: {
+    baseURL: 'http://localhost:8888',
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
+  },
+  retries: 0,
+  expect: {
+    toMatchSnapshot: {
+      maxDiffPixelRatio: 0.01,
+    },
+  },
+  webServer: [
+    {
+      command: 'yarn start',
+      url: 'http://localhost:8888/lab',
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
+  testDir: './tests',
+};
---
+++ ui-tests/tests/template.test.ts
@@ -0,0 +1,38 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+import { expect, test, APIRequestContext, Page } from '@playwright/test';
+
+async function beforeEach(
+  page: Page,
+  request: APIRequestContext
+): Promise<void> {
+  page.setDefaultTimeout(600000);
+  page.setViewportSize({ width: 1920, height: 1080 });
+  page.on('console', (message) => {
+    console.log('CONSOLE MSG ---', message.text());
+  });
+}
+
+test.describe('Template tests', () => {
+  test.beforeEach(async ({ page, request }) => void beforeEach(page, request));
+
+  test('Render iris_example.ipynb', async ({ page }) => {
+    const notebookName = 'iris_example';
+    // const page = await context.newPage();
+    await page.goto(`/voila/render/${notebookName}.ipynb?template=gridstack`),
+      // wait for the widgets to load
+      await page.waitForSelector('div.lm-Widget.jp-RenderedImage');
+    await page.waitForTimeout(500);
+    expect(await page.screenshot()).toMatchSnapshot(`${notebookName}.png`);
+  });
+
+  test('Render scotch_dashboard.ipynb', async ({ page }) => {
+    const notebookName = 'scotch_dashboard';
+    await page.goto(`/voila/render/${notebookName}.ipynb?template=gridstack`);
+    // wait for the widgets to load
+    await page.waitForSelector('div.lm-Widget.bqplot.figure.jupyter-widgets');
+    await page.waitForTimeout(500);
+    expect(await page.screenshot()).toMatchSnapshot(`${notebookName}.png`);
+  });
+});
